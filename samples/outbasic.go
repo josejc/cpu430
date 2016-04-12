e6b05569dfd9896e94e1ca836f4ba257b9aa2f11
José J. Cabezas <josej.cabezas@gmail.com>
2016-04-12 22:00:18 +0200

    Playing with termbox

--- samples/outbasic.go
+++ samples/outbasic.go
@@ -2,12 +2,6 @@ package main
 
 import "github.com/nsf/termbox-go"
 
-func draw() {
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-
-	termbox.Flush()
-}
-
 func main() {
 	err := termbox.Init()
 	if err != nil {
@@ -15,7 +9,8 @@ func main() {
 	}
 	defer termbox.Close()
 
-	draw()
+	setString(10, 2, "Initial presentation", termbox.ColorBlue, termbox.ColorBlack)
+	termbox.Flush()
 loop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
@@ -25,9 +20,23 @@ loop:
 				break loop
 			default:
 				draw()
+				termbox.Flush()
 			}
 		case termbox.EventResize:
-			draw()
+			termbox.Clear(termbox.ColorBlack, termbox.ColorBlack)
+			setString(10, 7, "Resize the window", termbox.ColorYellow, termbox.ColorBlack)
+			termbox.Flush()
 		}
 	}
+}
+
+func setString(x, y int, s string, fg, bg termbox.Attribute) {
+	for i, ch := range s {
+		termbox.SetCell(x+i, y, ch, fg, bg)
+	}
+}
+
+func draw() {
+	setString(10, 4, "Welcome to CPU430", termbox.ColorRed|termbox.AttrBold, termbox.ColorBlack)
+	setString(10, 5, "Press ESC key to exit.", termbox.ColorWhite, termbox.ColorBlack)
 }
