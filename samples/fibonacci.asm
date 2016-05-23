120a13bdcc2919f963bd58342f4c7d7fa6ada585
José J. Cabezas <josej@localhost.localdomain>
2016-05-23 22:32:33 +0200

    Simple remark

--- samples/fibonacci.asm
+++ samples/fibonacci.asm
@@ -1,3 +1,6 @@
+; 
+; Now I need modify for compile with naken ;)
+;
 ;-------------------------------------------------------------------------------
 ; MSP430 Assembler Code Template for use with TI Code Composer Studio
 ;
